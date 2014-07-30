03bee58b886f5c19ca9fd444746ab670e4ca8401
vincent leung <vincent.leung.cn@gmail.com>
2014-07-30 09:32:31 +0000

    backup

--- tools/delete_by_query.sh
+++ tools/delete_by_query.sh
@@ -1,6 +1,10 @@
-$ curl -XDELETE 'http://localhost:9200/twitter/tweet/_query' -d '{
-"query" : {
-"term" : { "user" : "kimchy" }
-				}
-			}
-			'
+curl -XGET 'http://localhost:9200/theegg/cosmopolitan/_query' -d '{
+	"query" : {
+		"term" : { "url" : "http://www.cosmopolitan.com.hk/fashion/fashion-features/playing-field" }
+	}
+}'
+curl -XGET 'http://localhost:9200/theegg/cosmopolitan/_search' -d '{
+	"query" : {
+		"term" : { "_id" : "4455"}
+	}
+}'
